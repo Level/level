bf3e7a6e4f30b0a439feb70a7061190054f39296
ZhangYuan <zyct@vip.qq.com>
2019-12-24 11:10:43 +0800

    support typescript

--- level.js
+++ level.js
@@ -1,1 +1,3 @@
-module.exports = require('level-packager')(require('leveldown'))
+const level = require('level-packager')(require('leveldown'))
+module.exports = level
+module.exports.default = level
