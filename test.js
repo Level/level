2c5ce1b1239acd1f9f33ba6850456fa8ed672a84
Vincent Weevers <mail@vincentweevers.nl>
2022-03-06 16:12:13 +0100

    Simplify test

--- test.js
+++ test.js
@@ -7,9 +7,8 @@ const { Level } = require('.')
 // Because we directly export classic-level or browser-level
 // without wrapping them, there's no need for further tests here.
 test('smoke test', async function (t) {
-  const db = new Level('db-' + uuid())
+  const db = new Level('db/' + uuid())
   await db.put('abc', '123')
   t.is(await db.get('abc'), '123')
-  await db.close()
-  await Level.destroy(db.location)
+  return db.close()
 })
